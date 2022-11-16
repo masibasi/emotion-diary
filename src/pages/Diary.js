9b357fab62ef089d82087861fe25f33f27837c66
masibasi <jimin_lee98@naver.com>
2022-11-17 06:42:45 +0900

    7-2 route test

---
+++ src/pages/Diary.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Diary = () => {
+    return (
+        <div>
+            <h1>Diary</h1>
+            <p>d이곳은 일기상세 페이지입니다.</p>
+        </div>
+    );
+};
+
+export default Diary;
