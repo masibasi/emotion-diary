c6b9fd911d530a0a83722f154986453484ada09c
masibasi <jimin_lee98@naver.com>
2022-11-21 08:35:11 +0900

    add context, dispatch, data context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useReducer, useRef } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -11,29 +11,86 @@ import Diary from "./pages/Diary";
 import MyButton from "./components/MyButton";
 import MyHeader from "./components/MyHeader";
 
+const reducer = (state, action) => {
+    let newState = [];
+    switch (action.type) {
+        case "INIT": {
+            return action.data;
+        }
+        case "CREATE": {
+            newState = [action.data, ...state];
+            break;
+        }
+        case "REMOVE": {
+            newState = state.filter((it) => it.id !== action.targetId);
+            break;
+        }
+        case "EDIT": {
+            newState = state.filter((it) =>
+                it.id === action.data.id ? { ...action.data } : it
+            );
+            break;
+        }
+        default:
+            return state;
+    }
+    return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
 function App() {
+    const [data, dispatch] = useReducer(reducer, []);
+
+    const dataId = useRef(0);
+
+    //CREATE
+    const onCreate = (date, content, emotion) => {
+        dispatch({
+            type: "CREATE",
+            data: {
+                id: dataId.current,
+                date: new Date(date).getTime,
+                content,
+                emotion,
+            },
+        });
+        dataId.current += 1;
+    };
+    //REMOVE
+    const onRemove = (targetId) => {
+        dispatch({ type: "REMOVE", targetId });
+    };
+    //EDIT
+    const onEdit = (targetID, date, content, emotion) => {
+        dispatch({
+            type: "EDIT",
+            data: {
+                id: targetID,
+                date: new Date(date).getTime,
+                content,
+                emotion,
+            },
+        });
+    };
     return (
-        <BrowserRouter>
-            <div className="App">
-                <MyHeader
-                    headText={"app.js"}
-                    leftChild={<MyButton text={"왼쪽 버튼"} />}
-                    rightChild={<MyButton text={"오른쪽 버튼"} />}
-                />
-                <h2>app.js</h2>
-                <MyButton text="hi" type="POSITIVE" />
-                <MyButton text="hi" type="NEGATIVE" />
-                <MyButton />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/new" element={<New />} />
-                    <Route path="/edit" element={<Edit />} />
-                    <Route path="/diary/:id" element={<Diary />} />
-                    // colon을 통해 id값을 전달 할 수있다. 대신 id를 무조건 받는
-                    형식이다
-                </Routes>
-            </div>
-        </BrowserRouter>
+        <DiaryStateContext.Provider value={data}>
+            <DiaryDispatchContext.Provider value={(onCreate, onEdit, onRemove)}>
+                <BrowserRouter>
+                    <div className="App">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/new" element={<New />} />
+                            <Route path="/edit" element={<Edit />} />
+                            <Route path="/diary/:id" element={<Diary />} />
+                            // colon을 통해 id값을 전달 할 수있다. 대신 id를
+                            무조건 받는 형식이다
+                        </Routes>
+                    </div>
+                </BrowserRouter>
+            </DiaryDispatchContext.Provider>
+        </DiaryStateContext.Provider>
     );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
-            <p>d이곳은 홈 입니다.</p>
+            <h1>JY Home</h1>
+            <p>d이곳은 JY 홈 입니다.</p>
         </div>
     );
 };
